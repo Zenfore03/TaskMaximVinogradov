12caacaee4299e863ef0507a11060547a6462312
Zenfore03 <vk223777@mail.ru>
2024-09-22 17:29:01 +0300

    Update task_08.py

--- task_08.py
+++ task_08.py
@@ -1,23 +1,29 @@
-import re
-
-def multiply_numbers(inputs):
-    if not isinstance(inputs, str):
-        inputs = str(inputs)
-    clean_string = re.sub(r'[^0-9]', '', inputs)
-    if not clean_string:
+def multiply_numbers(inputs= ""):
+    if isinstance(inputs, str):
+        digits = [int(d) for d in inputs if d.isdigit()]
+        if digits:
+            return multiply(digits)
+        else:
+            return None
+    elif isinstance(inputs, (int, float)):
+        return multiply_numbers(str(inputs))
+    elif isinstance(inputs, (list, tuple)):
+        return multiply(inputs)
+    else:
         return None
+
+def multiply(numbers):
     result = 1
-    for digit in map(int, clean_string):
-        result *= digit
+    for num in numbers:
+        result *= num
     return result
 
-print(multiply_numbers(None))
+print(multiply_numbers())
 print(multiply_numbers('ss'))
 print(multiply_numbers('1234'))
 print(multiply_numbers('sssdd34'))
 print(multiply_numbers(2.3))
 print(multiply_numbers([5, 6, 4]))
-
 
 
 

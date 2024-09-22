017d73b432d7dd4fe3f56e774f7cc26256025129
Zenfore03 <vk223777@mail.ru>
2024-09-22 17:34:57 +0300

    Update task_05.py

--- task_05.py
+++ task_05.py
@@ -1,11 +1,9 @@
 from datetime import datetime, timedelta
-
 def date_in_future(integer):
     if not isinstance(integer, int):
-        return datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+        return datetime.now().strftime('%d-%m-%Y %H:%M:%S')
     future_date = datetime.now() + timedelta(days = integer)
-    return future_date.strftime('%d.%m.%Y %H:%M:%S')
-
+    return future_date.strftime('%d-%m-%Y %H:%M:%S')
 print(date_in_future([]))
 print(date_in_future(2))
 

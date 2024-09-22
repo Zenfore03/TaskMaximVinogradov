7bff0ae0dfa345a68e39617b6df563e0c078ad02
Zenfore03 <vk223777@mail.ru>
2024-09-22 17:32:29 +0300

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -1,10 +1,8 @@
-import regex
-
 def is_palindrome(string):
     string = str(string)
     if not isinstance(string, str):
         return False
-    clean_string = regex.sub(r'[^a-zA-Z0-9]', '', str(string)).lower()
+    clean_string = ''.join(char.lower() for char in string if char.isalnum())
     return clean_string == clean_string[::-1]
 
 print(is_palindrome("A man, a plan, a canal -- Panama"))
@@ -12,8 +10,6 @@ print(is_palindrome("Madam, I'm Adam!"))
 print(is_palindrome(333))
 print(is_palindrome(None))
 print(is_palindrome("Abracadabra"))
-
-
 
 
 
